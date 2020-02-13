89e66b7930232c25a6814f5b17f2566cd8520979
TanTai92 <hanhduykaka@users.noreply.github.com>
2020-02-13 15:57:08 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import React, { Component } from 'react';
-import Todo from "./component/todo"
+import React from 'react';
+import Todos from './components/Todos'
+
 import './App.css';
 
-class App extends Component {
-  state={
-    todos:[
-      {
-        id:1,
-        title:"hihi",
-        completed:false
-      },   {
-        id:2,
-        title:"hai nha",
-        completed:false
-      },   {
-        id:3,
-        title:"3 nha",
-        completed:true
-      },   {
-        id:4,
-        title:"4 nhas",
-        completed:false
-      },   {
-        id:5,
-        title:"5 ne",
-        completed:false
-      }
-    ]
-  }
-  render() {
-    return (
-      <div className="App">
-      <Todo todos={this.state.todos}></Todo>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Todos></Todos>
+      </header>
+    </div>
+  );
 }
 
 export default App;
